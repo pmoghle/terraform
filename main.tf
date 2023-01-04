86d9e012da40dec8e0d37bc4c51b7d5d196b2ca9
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-04 09:49:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+
+    access_key = "${var.AKIAQPEKF2BZAWQMHXIZ}"
+
+    secret_key = "${var.pwDKlRjIIy7eAoFCvGo6CMTDwsL7xydLu3Y2HeOc}"
+
+    region = "${var.ap-south-1}"
+
+}
+
+module "s3" {
+
+    source = "<path-to-S3-folder>"
+
+    bucket_name = "pooja-bucket"       
+
+}
+
+resource "aws_s3_bucket" "temps3" {
+
+    bucket = "${var.pooja-bucket}" 
+
+    acl = "${var.acl_value}"   
+
+}
