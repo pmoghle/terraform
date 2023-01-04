1ba145626a34b4648de70a307afd2fe39a66a432
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-04 10:04:41 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "pooja-bucket" {}
+
+variable "acl_value" {
+
+    default = "private"
+
+}
+
+variable "AKIAQPEKF2BZAWQMHXIZ" {
+
+default = “<AKIAQPEKF2BZAWQMHXIZ>”
+
+}
+
+variable "pwDKlRjIIy7eAoFCvGo6CMTDwsL7xydLu3Y2HeOc" {
+
+default = “<your_secret_keypwDKlRjIIy7eAoFCvGo6CMTDwsL7xydLu3Y2HeOc>”
+
+ }
+
+variable "ap-south-1" {
+
+    default = "ap-south-1"
+
+}
